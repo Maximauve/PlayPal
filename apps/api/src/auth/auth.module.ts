8c3285cf359ac19bed952abbcb21ba12e61c5a25
f.garcia <fabiogarcia584@gmail.com>
2024-10-29 19:59:05 +0100

    feat(login): WIP add login module (#5)

---
+++ apps/api/src/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './services/auth.service';
+import { UsersModule } from '../user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { forwardRef } from '@nestjs/common/utils';
+
+@Module({
+    imports: [
+      forwardRef(() => UsersModule),
+      PassportModule,
+      JwtModule.register({
+        secret: process.env.JWT_SECRET,
+        signOptions: { expiresIn: '2629800s' },
+      }),
+    ],
+    providers: [AuthService, JwtStrategy],
+    exports: [AuthService, JwtStrategy],
+  })
+export class AuthModule {}
---
+++ apps/api/src/auth/exception-filter/exception-filter.ts
@@ -0,0 +1,14 @@
+import {Catch, ExceptionFilter, UnauthorizedException, ArgumentsHost} from '@nestjs/common';
+import {Response} from 'express';
+
+@Catch(UnauthorizedException)
+export class AuthExceptionFilter implements ExceptionFilter {
+  catch(exception: UnauthorizedException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    response.status(401).json({
+      statusCode: 401,
+      message: 'Tu n\'es pas connecté',
+    });
+  }
+}
