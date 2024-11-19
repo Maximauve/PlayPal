aca69c01404f7eedd7d234c5eb512fc9b0e80676
Mattox0 <matbegaaar@gmail.com>
2024-11-19 09:17:13 +0100

    feat(images): make tags not nullable

--- apps/api/src/game/game.entity.ts
+++ apps/api/src/game/game.entity.ts
@@ -39,6 +39,6 @@ export class Game {
   @OneToMany(() => Product, product => product.game)
   product?: Product[];
 
-  @ManyToMany(() => Tag, tag => tag.games, { nullable: true })
+  @ManyToMany(() => Tag, tag => tag.games)
   tags: Tag[];
 }
