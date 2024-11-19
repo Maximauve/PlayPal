4b2c12b69bc4ec209528cd4544c905ba1a594d79
Maximauve <max.mourgues@gmail.com>
2024-11-19 09:02:56 +0100

    feat(front/i18n): Initialize i18n

---
+++ apps/frontend/src/assets/translations/fr-FR/index.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment -- This is json, so we must ignore the rule for this time */
+import common from '@/assets/translations/fr-FR/common.json';
+import home from '@/assets/translations/fr-FR/home.json';
+
+const FR = {
+  ...common,
+  ...home,
+};
+
+export default FR;
---
+++ apps/frontend/src/assets/translations/index.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment -- This is json, so we must ignore the rule for this time */
+import FR from '@/assets/translations/fr-FR';
+
+const translations = {
+  "fr-FR": FR,
+};
+
+export default translations;
