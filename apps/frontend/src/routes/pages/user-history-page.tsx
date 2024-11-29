6b6e61952dc3d837abfc043156533b19b35a0696
Fabio Garcia <fabiogarcia584@gmail.com>
2024-11-29 21:42:52 +0100

    feat(front/user pages): user pages WIP

---
+++ apps/frontend/src/routes/pages/user-history-page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function UserHistoryPage(): React.JSX.Element {
+  return (
+    <div className="user-history-page text-4xl">
+      <h1>User History Page</h1>
+    </div>
+  );
+}
---
+++ apps/frontend/src/routes/pages/user-profile-page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function UserProfilePage(): React.JSX.Element {
+  return (
+    <div className="user-profile-page w-full h-full flex align-center justify-center text-4xl">
+      <div>User Profile Page</div>
+    </div>
+  );
+}
