a9633496de3d8d0a2210ebeac123633573fd390e
Fabio Garcia <fabiogarcia584@gmail.com>
2024-11-29 21:42:52 +0100

    feat(front/user pages): user pages WIP

--- apps/frontend/src/components/_layout/user-page-sidebar.tsx
+++ apps/frontend/src/components/_layout/user-page-sidebar.tsx
@@ -5,10 +5,10 @@ export default function UserPageSidebar(): React.JSX.Element {
 
   return (
     <div className="user-page-sidebar h-full bg-neutral-700 text-2xl">
-      <NavLink to="/user/profile" className={({ isActive }) => isActive ? "active-user-nav-item" : ""}  >
+      <NavLink to="/user/1/profile" className={({ isActive }) => isActive ? "active-user-nav-item" : ""}  >
         <div className="flex justify-center items-center h-24 w-full border-b-2 border-slate-50 hover:bg-cyan-600"> Edit Profil</div>
       </NavLink>
-      <NavLink to="/user/history" className={({ isActive }) => isActive ? "active-user-nav-item" : ""} >
+      <NavLink to="/user/1/history" className={({ isActive }) => isActive ? "active-user-nav-item" : ""} >
         <div className="flex justify-center items-center h-24 w-full border-b-2 border-slate-50 hover:bg-cyan-600"> History</div>
       </NavLink>
     </div>
