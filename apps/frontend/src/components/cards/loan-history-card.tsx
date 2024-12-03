a2e97c9d39237bdbde10dc17574d4484bd3a32f2
Fabio Garcia <fabiogarcia584@gmail.com>
2024-12-03 21:55:46 +0100

    feat(front/user pages): user page history done

--- apps/frontend/src/components/cards/loan-history-card.tsx
+++ apps/frontend/src/components/cards/loan-history-card.tsx
@@ -9,9 +9,9 @@ interface Props {
   loan: Loan;
 }
 
-export default function LoanHistoryCard({ loan } : Props): React.JSX.Element {
+export default function LoanHistoryCard({ loan }: Props): React.JSX.Element {
 
-  const game: Game |null  = loan.product?.game ?? null;
+  const game: Game | null = loan.product?.game ?? null;
   const inStock: boolean = loan.product?.available ?? false;
   const i18n = useTranslation();
 
@@ -28,9 +28,13 @@ export default function LoanHistoryCard({ loan } : Props): React.JSX.Element {
       />
       <div className="p-4 flex flex-col w-full">
         <div className="flex items-start justify-between">
-          <div className="flex-1">
+          <div className="flex flex-col">
             <h3 className="text-lg font-bold truncate text-black">{game?.name}</h3>
-            <p className="text-gray-500 font-medium">{game?.brand}</p>
+            <p className="text-gray-500">{game?.brand}</p>
+          </div>
+          <div className="p-4 flex flex-col justify-center">
+            <p className="text-gray-500 font-medium">{i18n.t("card.loanStartDate")} {new Date(loan.startDate).toLocaleString()}</p>
+            <p className="text-gray-500 font-medium">{i18n.t("card.loanEndDate")} {new Date(loan.endDate).toLocaleString()}</p>
           </div>
           <span className={`${inStock ? 'bg-green-700' : 'bg-red-600'} text-white text-xs px-2 py-1 rounded`}>{inStock ? i18n.t("card.inStock") : i18n.t("card.notStock")}</span>
         </div>
@@ -38,12 +42,12 @@ export default function LoanHistoryCard({ loan } : Props): React.JSX.Element {
           <Rating rating={1.2} />
           <span className="text-gray-600 text-sm"> - {game?.rating?.length ?? 0} {i18n.t("card.rating")}</span>
         </div>
-        <TagsContainer tags={game?.tags ?? []}/>
+        <TagsContainer tags={game?.tags ?? []} />
         <div className="flex justify-start relative mb-3 mt-auto pl-3">
           <button className="w-1/5 bg-black p-3 rounded-md">{i18n.t("card.open")}</button>
         </div>
       </div>
-      
+
     </div>
-  );   
+  );
 }
--- apps/frontend/src/routes/pages/user-history-page.tsx
+++ apps/frontend/src/routes/pages/user-history-page.tsx
@@ -8,7 +8,7 @@ export default function UserHistoryPage(): React.JSX.Element {
   console.log(data);
   
   return (
-    <div className="user-history-page text-4xl ">
+    <div className="user-history-page p-6 ">
       {data && data?.map((loan) => (
         <div key={loan.id} className="m-6">
           <LoanHistoryCard loan={loan} />
