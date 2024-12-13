116e0bb3b8c939e9ff20a7eab289edb088221d89
Maximauve <max.mourgues@gmail.com>
2024-12-13 02:38:16 +0100

    feat(selectInput): Stylized selectInput

--- apps/frontend/src/components/form/select-input.tsx
+++ apps/frontend/src/components/form/select-input.tsx
@@ -1,10 +1,6 @@
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-<<<<<<< HEAD
-import { useEffect, useState } from "react";
-=======
-import { useMemo, useState } from "react";
->>>>>>> 80ed8a8 (feat(admin/products): WIP - init product creation & select input style)
+import { useState } from "react";
 
 import { type WordingKey } from "@/context/i18n/i18n-service";
 import useTranslation from "@/hooks/use-translation";
@@ -29,49 +25,42 @@ export default function SelectInput({
   label,
   error,
   isMultiple = false,
-  // required = false,
 }: SelectProps) {
   const i18n = useTranslation();
   const [currentValue, setCurrentValues] = useState<typeof value>(value);
   const [isOpen, setIsOpen] = useState(false);
 
-  const handleClick = (checkedItem: string) => {
+  const handleClick = (checkedItem: { label: string; value: string }) => {
     if (isMultiple) {
-      if ((currentValue as string[]).includes(checkedItem)) {
-        setCurrentValues(prevValues => (prevValues as string[]).filter(v => v !== checkedItem));
+      if ((currentValue as string[]).includes(checkedItem.value)) {
+        setCurrentValues(prevValues => (prevValues as string[]).filter(v => v !== checkedItem.value));
       } else {
-        setCurrentValues(prevValues => [...(prevValues as string[]), checkedItem]);
+        setCurrentValues(prevValues => [...(prevValues as string[]), checkedItem.value]);
       }
       (onChange as ((value: string[]) => void))(currentValue as string[]);
     } else {
-      setCurrentValues(checkedItem);
-      (onChange as ((value: string) => void))(currentValue as string);
+      setCurrentValues(checkedItem.value);
+      if (onChange.length === 1) {
+        (onChange as ((value: string) => void))(checkedItem.value);
+      } else if (onChange.length === 2) {
+        (onChange as (field: string, newValue: string) => void)(name, checkedItem.value);
+      }
     }
   };
 
-<<<<<<< HEAD
-  useEffect(() => {
-    console.log(currentValue);
-  }, [currentValue]);
-
-=======
->>>>>>> 80ed8a8 (feat(admin/products): WIP - init product creation & select input style)
   const toggleSelection = () => {
     setIsOpen(prev => !prev);
   };
 
+  const selectedLabel = options.find(option => option.value === currentValue)?.label || '';
+
   return (
     <div className="flex flex-col items-start w-full px-5">
-<<<<<<< HEAD
       {label && (
-=======
-      {!isMultiple && label && (
->>>>>>> 80ed8a8 (feat(admin/products): WIP - init product creation & select input style)
         <p className="block text-sm font-medium text-gray-700">
           {i18n.t(label)}
         </p>
       )}
-<<<<<<< HEAD
       <div className="w-full relative">
         <button type="button" onClick={toggleSelection}
           className={"w-full max-w-full m-0 bg-transparent border border-gray-400 rounded-sm py-1 pl-2 flex flex-row justify-between items-center" + (isOpen && ('rounded-t-md border-b-0 '))}
@@ -79,7 +68,7 @@ export default function SelectInput({
           <span>
             {isMultiple 
               ? (label && ((i18n.t(label) + ((currentValue.length > 0) ? ` (${currentValue.length})` : ''))))
-              : (currentValue ?? label)
+              : selectedLabel 
             }
           </span>
           <FontAwesomeIcon icon={faChevronDown} width={22} height={22} color="black" className={'transition-transform duration-150' + (isOpen && ' rotate-180')} />
@@ -93,31 +82,7 @@ export default function SelectInput({
                 return (
                   <li className={index === options.length ? '' : 'mb-1'} key={option.value}>
                     <label>
-                      <input className="mr-2" type={isMultiple ? 'checkbox' : 'radio'} name={name} value={option.value} onChange={() => handleClick(option.value)} checked={checked} />
-=======
-      <div className="w-full">
-        <button onClick={toggleSelection}
-          className={"w-full max-w-full m-0 bg-transparent border border-gray-300 rounded-sm py-1 pl-2 flex justify-between items-center" + isOpen && ('rounded-t-md border-b-0 ')}
-        >
-          <span>
-            {isMultiple 
-              ? (label && (i18n.t(label) + (currentValue.length > 0) && ` (${currentValue.length})`))
-              : (currentValue ?? label)
-            }
-          </span>
-          <FontAwesomeIcon icon={faChevronDown} width={22} height={22} color="black" className={'transition-transform duration-150' + isOpen && 'rotate-180'} />
-        </button>
-        <div>
-          <div>
-            <ul>
-              {options.map((option) => {
-                let checked = false;
-                checked = isMultiple && Array.isArray(value) ? value.some(v => valuesOptions.includes(v)) : valuesOptions.includes(value as string);
-                return (
-                  <li>
-                    <label>
-                      <input type={isMultiple ? 'checkbox' : 'radio'} key={option.value} name={name} value={option.value} onChange={() => handleClick(option.value)} checked={checked} />
->>>>>>> 80ed8a8 (feat(admin/products): WIP - init product creation & select input style)
+                      <input className="mr-2" type={isMultiple ? 'checkbox' : 'radio'} name={name} value={option.value} onChange={() => handleClick(option)} checked={checked} />
                       {option.label}
                     </label>
                   </li>
@@ -127,24 +92,6 @@ export default function SelectInput({
           </div>
         </div>
       </div>
-      {/* <select
-        id={id}
-        name={name}
-        multiple
-        value={value}
-        onChange={handleSelectChange}
-        className={`block w-full px-3 py-2 border rounded-md shadow-sm sm:text-sm ${
-          error?.isError
-            ? "border-red-500 focus:ring-red-500 focus:border-red-500"
-            : "border-gray-300 focus:ring-indigo-500 focus:border-indigo-500"
-        }`}
-      >
-        {options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </select> */}
       
       {error?.isError && (
         <p className="text-red-600 text-sm w-full">{i18n.t(error.message)}</p>
